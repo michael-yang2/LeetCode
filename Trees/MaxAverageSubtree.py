aa5e4d6c7cd061f32d95bcd3fc2ddab980199fca
michael-yang2 <michaelyang2k8@gmail.com>
2020-09-30 20:46:31 -0700

    MaxAvgSSubtree

--- Trees/MaxAverageSubtree.py
+++ Trees/MaxAverageSubtree.py
@@ -5,22 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def avg_at_node(self, node):
-            if not node:
-                return (-sys.maxsize, 0)
-            curr_avg = node.val
-            curr_count = 1
-            left_avg, left_count = self.avg_at_node(node.left)
-            right_avg, right_count = self.avg_at_node(node.right)
-            if left_count != 0:
-                curr_avg = (left_avg*left_count + node.val)/(left_count+curr_count)
-                curr_count+=left_count
-            if right_count != 0:
-                curr_avg = ((curr_avg*curr_count)+right_avg*right_count)/(curr_count+right_count)
-                curr_count+=right_count
-            self.max_avg = max((self.max_avg, left_avg, right_avg, curr_avg))
-            return (curr_avg, curr_count)
     def maximumAverageSubtree(self, root: TreeNode) -> float:
         self.max_avg = -sys.maxsize
-        a, b = self.avg_at_node(root)
+        def recur(node):
+            if not node:
+                return (-sys.maxsize, 0)
+            curr_sum = node.val
+            curr_count = 1
+            left_sum, left_count = recur(node.left)
+            right_sum, right_count = recur(node.right)
+            left_avg = -sys.maxsize
+            right_avg = -sys.maxsize
+            if left_count != 0:
+                curr_sum+=left_sum
+                curr_count+=left_count
+                left_avg = left_sum/left_count
+            if right_count != 0:
+                curr_sum+=right_sum
+                curr_count+=right_count
+                right_avg = right_sum/right_count
+            self.max_avg = max((self.max_avg, curr_sum/curr_count, left_avg, right_avg))
+            return (curr_sum, curr_count)
+        recur(root)
         return self.max_avg
